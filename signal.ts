d430af0fd0c15948b02e925ee05645c7ea3b45b4
Rainer Hahnekamp <rainer.hahnekamp@gmail.com>
2023-09-22 19:06:27 +0200

    add reactive computed

--- signal.ts
+++ signal.ts
@@ -1,16 +1,75 @@
-export type Signal<T> = (() => T) & {
-  set(newValue: T): void;
-  update(updateFn: (value: T) => T): void;
+export interface ReactiveNode<T> {
+  computation?(): T;
+  consumers: ReactiveNode<unknown>[];
+  producerHasChanged: () => void;
+  value: T;
+}
+
+let activeConsumer: ReactiveNode<unknown> | undefined;
+
+export type Signal<T> = {
+  (): T;
 };
 
-export function signal<T>(initialValue: T): Signal<T> {
-  let value = initialValue;
+export interface WritableSignal<T> extends Signal<T> {
+  set(newValue: T): void;
+  update(updateFn: (value: T) => T): void;
+}
+
+export function signal<T>(initialValue: T): WritableSignal<T> {
+  const node: ReactiveNode<T> = {
+    consumers: [],
+    value: initialValue,
+    producerHasChanged: () => {},
+  };
 
   function signalFn() {
-    return value;
+    if (activeConsumer && !node.consumers.includes(activeConsumer)) {
+      node.consumers.push(activeConsumer);
+    }
+    return node.value;
   }
-  signalFn.set = (newValue: T) => (value = newValue);
-  signalFn.update = (cb: (value: T) => T) => (value = cb(value));
+  signalFn.set = (newValue: T) => {
+    node.value = newValue;
+    producerNotifyConsumers(node);
+  };
+  signalFn.update = (cb: (value: T) => T) => {
+    node.value = cb(node.value);
+    producerNotifyConsumers(node);
+  };
 
   return signalFn;
+}
+
+function producerNotifyConsumers(node: ReactiveNode<unknown>) {
+  for (const consumer of node.consumers) {
+    consumer.producerHasChanged();
+  }
+}
+
+const UNSET = Symbol("UNSET");
+
+export function computed<T>(computation: () => T): Signal<T> {
+  const node: ReactiveNode<T | typeof UNSET> = {
+    computation,
+    consumers: [],
+    value: UNSET,
+    producerHasChanged: () => {
+      node.value = node.computation();
+    },
+  };
+
+  function computed() {
+    let prevConsumer = activeConsumer;
+    activeConsumer = node;
+    if (node.value === UNSET) {
+      node.value = node.computation();
+    }
+
+    activeConsumer = prevConsumer;
+
+    return node.value;
+  }
+
+  return computed as Signal<T>;
 }
--- signals.spec.ts
+++ signals.spec.ts
@@ -1,5 +1,5 @@
-import { describe, expect, it } from "vitest";
-import { signal } from "./signal";
+import { describe, expect, it, test } from "vitest";
+import { computed, signal } from "./signal";
 
 describe("Signals", () => {
   it("should create a signal", () => {
@@ -14,4 +14,31 @@ describe("Signals", () => {
       return value;
     });
   });
+
+  describe("computed", () => {
+    test("computed should produce a derived value", () => {
+      const person = signal({ firstname: "Jessica", name: "Smith" });
+      const prettyName = computed(
+        () => `${person().firstname} ${person().name}`,
+      );
+      expect(prettyName()).toBe("Jessica Smith");
+    });
+
+    test("computed should be reactive", () => {
+      let computedCount = 0;
+      const person = signal({ firstname: "Jessica", name: "Smith" });
+      const prettyName = computed(() => {
+        computedCount++;
+        return `${person().firstname} ${person().name}`;
+      });
+      expect(prettyName()).toBe("Jessica Smith");
+      person.update((value) => ({ ...value, name: "Johnson" }));
+      expect(prettyName()).toBe("Jessica Johnson");
+      expect(prettyName()).toBe("Jessica Johnson");
+
+      expect(computedCount).toBe(2);
+    });
+  });
+
+  test.skip("computed should be a producer as well");
 });
